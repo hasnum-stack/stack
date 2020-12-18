74b7a69a1d22207d50185338a98a8daa5f1cd876
hasnum <hasnum@163.com>
2020-12-18 19:29:14 +0800

    1

--- express/test1/routes/index.js
+++ express/test1/routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var urlParseLax = require('url-parse-lax');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
+    console.log(urlParseLax('https://www.baidu.com'));
     res.render('index', { title: 'Express' });
 });
 
---
+++ reactantd/src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import { Button, Space, Upload, Popconfirm } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import './App.css';
+
+function App() {
+    return (
+        <Space>
+            Space
+            <Button type="primary">Button</Button>
+            <Upload>
+                <Button>
+                    <UploadOutlined /> Click to Upload
+                </Button>
+            </Upload>
+            <Popconfirm title="Are you sure delete this task?" okText="Yes" cancelText="No">
+                <Button>Confirm</Button>
+            </Popconfirm>
+        </Space>
+    );
+}
+
+export default App;
