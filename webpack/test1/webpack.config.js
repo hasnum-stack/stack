4b6a2236dd9449595237a08c1de059bbf784cca8
hasnum-stack <hasnum@163.com>
2021-01-03 14:10:58 +0800

    style

--- webpack/test1/webpack.config.js
+++ webpack/test1/webpack.config.js
@@ -1,15 +1,38 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
     mode: 'development',
-    entry: './app.js',
+    entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'test.js',
+        filename: '[name].[fullhash:8].js',
     },
+    devtool: 'inline-source-map',
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public/index.html'),
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.(css|s[ac]ss)$/i,
+                use: [
+                    // 将 JS 字符串生成为 style 节点
+                    'style-loader',
+                    // 将 CSS 转化成 CommonJS 模块
+                    'css-loader',
+                    // 将 Sass 编译成 CSS
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
 };
