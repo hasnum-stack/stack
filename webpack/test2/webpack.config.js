d953464e53a154bd29a0fe74b1436e91cd6c6956
hasnum <hasnum@163.com>
2021-02-15 00:43:08 +0800

    1

--- webpack/test2/webpack.config.js
+++ webpack/test2/webpack.config.js
@@ -1,9 +1,39 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const __directory = [
+    /**
+     * 索引:0
+     * 关于Object.defineProperty的例子
+     *
+     */
+    {
+        entry: './src/definePropertyExample/index.js',
+        html: 'src/definePropertyExample/public/index.html',
+    },
+    /**
+     * 索引:1
+     * 关于双向绑定
+     *
+     */
+
+    {
+        entry: './src/v-model/index.js',
+        html: 'src/v-model/public/index.html',
+    },
+];
+
+/**
+ * 切换应用
+ *
+ */
+const __position = __directory[1];
+
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+
+    entry: __position['entry'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.[contenthash:8].js',
@@ -22,7 +52,7 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html'),
+            template: path.resolve(__dirname, __position['html']),
         }),
     ],
 };
