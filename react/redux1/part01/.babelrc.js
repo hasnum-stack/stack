5e9422be5f94dc5b70b43db6e734c5821ee083d1
hasnum-stack <hasnum@163.com>
2021-07-05 01:19:52 +0800

    redux优化

--- react/redux1/part01/.babelrc.js
+++ react/redux1/part01/.babelrc.js
@@ -1,11 +1,13 @@
 module.exports = {
+  presets: ['react-app'],
   plugins: [
     [
       'import',
+      //antd/es/${component}/style/css.js
       {
         libraryName: 'antd',
         libraryDirectory: 'es',
-        style: true,
+        style: 'css',
       },
     ],
   ],
--- react/redux1/part01/src/App2.js
+++ react/redux1/part01/src/App2.js
@@ -1,35 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Button } from 'antd';
-// import 'antd/dist/antd.css';
+import { Button, Space, Typography, Row, Col, Input } from 'antd';
 import { plusActionModule1, minusActionModule1, asyncPlusActionModule1 } from './store/actions/module1';
 class App2 extends React.Component {
   render() {
     const { value, plusActionModule1, minusActionModule1, asyncPlusActionModule1 } = this.props;
     return (
       <>
-        <div>
-          <div>模块一</div>
-          <div>{value}</div>
-          <Button
-            onClick={() => {
-              plusActionModule1(1);
-            }}>
-            点击 + 1
-          </Button>
-          <Button
-            onClick={() => {
-              minusActionModule1(1);
-            }}>
-            点击 - 1
-          </Button>
-          <Button
-            onClick={() => {
-              asyncPlusActionModule1(1);
-            }}>
-            异步添加 + 1
-          </Button>
-        </div>
+        <Row justify="center">
+          <Col>
+            <div>模块一</div>
+            <Typography.Title
+              style={{
+                textAlign: 'center',
+              }}>
+              {value}
+            </Typography.Title>
+            <Space>
+              <Button
+                type="primary"
+                onClick={() => {
+                  plusActionModule1(1);
+                }}>
+                点击 + 1
+              </Button>
+              <Button
+                danger
+                type="primary"
+                onClick={() => {
+                  minusActionModule1(1);
+                }}>
+                点击 - 1
+              </Button>
+              <Button
+                onClick={() => {
+                  asyncPlusActionModule1(1);
+                }}>
+                异步添加 + 1
+              </Button>
+            </Space>
+          </Col>
+        </Row>
         <hr />
         <div>
           模块二
