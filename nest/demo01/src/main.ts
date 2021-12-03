3f09d125fed57ab4cfdddbc31da6fc264d3652bd
hasnum <hasnum@163.com>
2021-12-03 16:37:26 +0800

    feat: 设置跨域字段

--- nest/demo01/src/main.ts
+++ nest/demo01/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
+
   await app.listen(3000);
 }
 bootstrap();
